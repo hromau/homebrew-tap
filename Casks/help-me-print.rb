42a596e4ecd56d4b6b4f2d66c98947f02a97990e
hromau <31735278+hromau@users.noreply.github.com>
2026-06-24 10:56:21 +0200

    Add download URL for Help Me Print version 0.1.8

--- Casks/help-me-print.rb
+++ Casks/help-me-print.rb
@@ -2,7 +2,7 @@ cask "help-me-print" do
   version "0.1.8"
   sha256 "88ecb3973128f1b6b2d180247eb810d081fcd331d4edd30daf85d282e3ef3801"
 
-  url ""
+  url "https://github.com/hromau/help-me-print/releases/download/v0.1.8/help-me-print-0.1.8-Darwin.dmg"
   name "Help Me Print"
   desc "Manual duplex printing for printers without automatic double-sided support"
   homepage "https://github.com/hromau/help-me-print"
